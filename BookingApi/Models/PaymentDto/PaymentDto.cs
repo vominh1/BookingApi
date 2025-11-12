e753957100333384e63ad12b2e1941be90a3bc5f
vominh1 <vominhday1@gmail.com>
2025-11-12 22:33:02 +0700

    Add PaymentDto for payment processing API

---
+++ BookingApi/Models/PaymentDto/PaymentDto.cs
@@ -0,0 +1,7 @@
+﻿public class PaymentDto
+{
+    public string BookingCode { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public string? PaymentMethod { get; set; }
+    public string? TransactionRef { get; set; }
+}
