00116259028f00218a5f7faaec4341c360375de2
vominh1 <vominhday1@gmail.com>
2025-11-13 21:07:06 +0700

    thay đổi tên thư mục

--- BookingApi/Models/PaymentDto/PaymentDto.cs
+++
@@ -1,7 +0,0 @@
-﻿public class PaymentDto
-{
-    public string BookingCode { get; set; } = null!;
-    public decimal Amount { get; set; }
-    public string? PaymentMethod { get; set; }
-    public string? TransactionRef { get; set; }
-}
