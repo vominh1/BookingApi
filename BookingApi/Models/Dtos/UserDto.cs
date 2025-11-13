b188a373f7fb191a835d338ca939f977b4f3320a
vominh1 <vominhday1@gmail.com>
2025-11-13 21:05:09 +0700

    Tạo trung gian giao tiếp frontend và backend

---
+++ BookingApi/Models/Dtos/UserDto.cs
@@ -0,0 +1,27 @@
+﻿namespace BookingApi.DTOs
+{
+    public class CreateUserDto
+    {
+        public string Username { get; set; } = null!;
+        public string? Email { get; set; }
+        public string Password { get; set; } = null!;
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public int[] RoleIds { get; set; } = Array.Empty<int>();
+    }
+
+    public class LoginDto
+    {
+        public string UsernameOrEmail { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+    public class UserViewDto
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+        public string[] Roles { get; set; } = Array.Empty<string>();
+    }
+}
